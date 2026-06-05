a9baf63474a065ebbbccd000744d003aa9246c68
sebastianvasquezechavarria1234 <sebasvasquez1314@gmail.com>
2026-06-05 15:10:58 -0500

    refactor: rename project to vehicle-hierarchy, update namespace to VehicleHierarchy

--- tallerHoyMiercoles/Vehiculo.cs
+++ tallerHoyMiercoles/Vehiculo.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace tallerHoyMiercoles
+namespace VehicleHierarchy
 {
     [Serializable] // 👈 Esto indica que la clase puede ser serializada
     internal class Vehiculo
